0735a14769155a2ec0ed9b21c512f41baa2c1e21
Byun0904 <yja05092@gmail.com>
2022-04-01 22:34:58 +0900

    상세보기

--- main.js
+++ main.js
@@ -3,14 +3,42 @@ const app = express();
 const port = 3000;
 const fs = require("fs");
 const template = require("./lib/template.js");
+const sanitizeHtml = require("sanitize-html");
 
 app.get("/", (request, response) => {
   fs.readdir("./data", function (error, filelist) {
-    var title = "Welcome";
-    var description = "Hello, Node.js";
-    var list = template.list(filelist);
-    var html = template.HTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
+    const title = "Welcome";
+    const description = "Hello, Node.js";
+    const list = template.list(filelist);
+    const html = template.HTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
     response.send(html);
+  });
+});
+
+app.get("/page/:pageId", (request, respones) => {
+  fs.readdir("./data", function (error, filelist) {
+    fs.readFile(`data/${request.params.pageId}`, "utf8", function (err, description) {
+      const title = request.params.pageId;
+      const sanitizedTitle = sanitizeHtml(title);
+      const sanitizedDescription = sanitizeHtml(description, {
+        allowedTags: ["h1"],
+      });
+      const list = template.list(filelist);
+      const html = template.HTML(
+        sanitizedTitle,
+        list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        `
+        <a href="/create">create</a>
+        <a href="/update?id=${sanitizedTitle}">update</a>
+        <form action="delete_process" method="post">
+          <input type="hidden" name="id" value="${sanitizedTitle}">
+          <input type="submit" value="delete">
+        </form>
+        `
+      );
+      respones.send(html);
+    });
   });
 });
 
@@ -31,41 +59,7 @@ app.listen(port, () => {
 //   var queryData = url.parse(_url, true).query;
 //   var pathname = url.parse(_url, true).pathname;
 //   if (pathname === "/") {
-//     if (queryData.id === undefined) {
-//       fs.readdir("./data", function (error, filelist) {
-//         var title = "Welcome";
-//         var description = "Hello, Node.js";
-//         var list = template.list(filelist);
-//         var html = template.HTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
-//         response.writeHead(200);
-//         response.end(html);
-//       });
 //     } else {
-//       fs.readdir("./data", function (error, filelist) {
-//         var filteredId = path.parse(queryData.id).base;
-//         fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-//           var title = queryData.id;
-//           var sanitizedTitle = sanitizeHtml(title);
-//           var sanitizedDescription = sanitizeHtml(description, {
-//             allowedTags: ["h1"],
-//           });
-//           var list = template.list(filelist);
-//           var html = template.HTML(
-//             sanitizedTitle,
-//             list,
-//             `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-//             ` <a href="/create">create</a>
-//                 <a href="/update?id=${sanitizedTitle}">update</a>
-//                 <form action="delete_process" method="post">
-//                   <input type="hidden" name="id" value="${sanitizedTitle}">
-//                   <input type="submit" value="delete">
-//                 </form>`
-//           );
-//           response.writeHead(200);
-//           response.end(html);
-//         });
-//       });
-//     }
 //   } else if (pathname === "/create") {
 //     fs.readdir("./data", function (error, filelist) {
 //       var title = "WEB - create";
