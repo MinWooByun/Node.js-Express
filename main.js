17e0d687ebe77f848af21673e64bee7506b4ee9b
Byun0904 <yja05092@gmail.com>
2022-04-07 21:37:33 +0900

    미들웨어 만들기

--- main.js
+++ main.js
@@ -9,31 +9,35 @@ const compression = require("compression");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
-
-app.get("/", (request, response) => {
+// 미들웨어 만들기 get 방식중에 모두에 해당됨.
+app.get("*", (request, response, next) => {
   fs.readdir("./data", function (error, filelist) {
-    const title = "Welcome";
-    const description = "Hello, Node.js";
-    const list = template.list(filelist);
-    const html = template.HTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
-    response.send(html);
+    request.list = filelist;
+    next();
   });
 });
 
+app.get("/", (request, response) => {
+  const title = "Welcome";
+  const description = "Hello, Node.js";
+  const list = template.list(request.list);
+  const html = template.HTML(title, list, `<h2>${title}</h2>${description}`, `<a href="/create">create</a>`);
+  response.send(html);
+});
+
 app.get("/page/:pageId", (request, response) => {
-  fs.readdir("./data", function (error, filelist) {
-    fs.readFile(`data/${request.params.pageId}`, "utf8", function (err, description) {
-      const title = request.params.pageId;
-      const sanitizedTitle = sanitizeHtml(title);
-      const sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ["h1"],
-      });
-      const list = template.list(filelist);
-      const html = template.HTML(
-        sanitizedTitle,
-        list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        `
+  fs.readFile(`data/${request.params.pageId}`, "utf8", function (err, description) {
+    const title = request.params.pageId;
+    const sanitizedTitle = sanitizeHtml(title);
+    const sanitizedDescription = sanitizeHtml(description, {
+      allowedTags: ["h1"],
+    });
+    const list = template.list(request.list);
+    const html = template.HTML(
+      sanitizedTitle,
+      list,
+      `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+      `
         <a href="/create">create</a>
         <a href="/update/${sanitizedTitle}">update</a>
         <form action="/delete_process" method="post">
@@ -41,20 +45,18 @@ app.get("/page/:pageId", (request, response) => {
           <input type="submit" value="delete">
         </form>
         `
-      );
-      response.send(html);
-    });
+    );
+    response.send(html);
   });
 });
 
 app.get("/create", (request, response) => {
-  fs.readdir("./data", function (error, filelist) {
-    const title = "WEB - create";
-    const list = template.list(filelist);
-    const html = template.HTML(
-      title,
-      list,
-      `
+  const title = "WEB - create";
+  const list = template.list(request.list);
+  const html = template.HTML(
+    title,
+    list,
+    `
       <form action="/create_process" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
         <p>
@@ -65,10 +67,9 @@ app.get("/create", (request, response) => {
         </p>
       </form>
     `,
-      ""
-    );
-    response.send(html);
-  });
+    ""
+  );
+  response.send(html);
 });
 
 // create_process 말고 create form에서 주소를 create만 주고 post 방식이기 때문에 create만 적어줘도 된다.
@@ -83,15 +84,14 @@ app.post("/create_process", (request, response) => {
 });
 
 app.get("/update/:pageId", (request, response) => {
-  fs.readdir("./data", function (error, filelist) {
-    const filteredId = request.params.pageId;
-    fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-      const title = filteredId;
-      const list = template.list(filelist);
-      const html = template.HTML(
-        title,
-        list,
-        `
+  const filteredId = request.params.pageId;
+  fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
+    const title = filteredId;
+    const list = template.list(request.list);
+    const html = template.HTML(
+      title,
+      list,
+      `
         <form action="/update_process" method="post">
           <input type="hidden" name="id" value="${title}">
           <p><input type="text" name="title" placeholder="title" value="${title}"></p>
@@ -103,10 +103,9 @@ app.get("/update/:pageId", (request, response) => {
           </p>
         </form>
         `,
-        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-      );
-      response.send(html);
-    });
+      `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+    );
+    response.send(html);
   });
 });
 
