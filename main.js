50c98557ae60ed539309bc548fa899550412ede0
Byun0904 <yja05092@gmail.com>
2022-04-08 22:45:05 +0900

    라우터

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ const app = express();
 const port = 3000;
 const fs = require("fs");
 const template = require("./lib/template.js");
-const sanitizeHtml = require("sanitize-html");
 const bodyParser = require("body-parser");
 const compression = require("compression");
+const indexRouter = require("./routes/index");
+const topicRouter = require("./routes/topic");
 
 // public 디렉터리 안에서 static 파일을 찾겠다는 뜻이다.
 app.use(express.static("public"));
@@ -18,127 +19,10 @@ app.get("*", (request, response, next) => {
     next();
   });
 });
-
-app.get("/", (request, response) => {
-  const title = "Welcome";
-  const description = "Hello, Node.js";
-  const list = template.list(request.list);
-  const html = template.HTML(
-    title,
-    list,
-    `<h2>${title}</h2>${description}<img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px">`,
-    `<a href="/create">create</a>`
-  );
-  response.send(html);
-});
-
-app.get("/page/:pageId", (request, response, next) => {
-  fs.readFile(`data/${request.params.pageId}`, "utf8", function (err, description) {
-    if (err) {
-      next(err);
-    } else {
-      const title = request.params.pageId;
-      const sanitizedTitle = sanitizeHtml(title);
-      const sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ["h1"],
-      });
-      const list = template.list(request.list);
-      const html = template.HTML(
-        sanitizedTitle,
-        list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        `
-          <a href="/create">create</a>
-          <a href="/update/${sanitizedTitle}">update</a>
-          <form action="/delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>
-          `
-      );
-      response.send(html);
-    }
-  });
-});
-
-app.get("/create", (request, response) => {
-  const title = "WEB - create";
-  const list = template.list(request.list);
-  const html = template.HTML(
-    title,
-    list,
-    `
-      <form action="/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `,
-    ""
-  );
-  response.send(html);
-});
-
-// create_process 말고 create form에서 주소를 create만 주고 post 방식이기 때문에 create만 적어줘도 된다.
-// 이러면 get 방식이면 위에 코드가 실행되고 post 방식이면 아래의 코드가 실행된다.
-app.post("/create_process", (request, response) => {
-  const post = request.body;
-  const title = post.title;
-  const description = post.description;
-  fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-    response.redirect(`/page/${title}`);
-  });
-});
-
-app.get("/update/:pageId", (request, response) => {
-  const filteredId = request.params.pageId;
-  fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-    const title = filteredId;
-    const list = template.list(request.list);
-    const html = template.HTML(
-      title,
-      list,
-      `
-        <form action="/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-      `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-    );
-    response.send(html);
-  });
-});
-
-app.post("/update_process", (request, response) => {
-  const post = request.body;
-  const id = post.id;
-  const title = post.title;
-  const description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function (error) {
-    fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-      response.redirect(`/page/${title}`);
-    });
-  });
-});
-
-app.post("/delete_process", (request, response) => {
-  const post = request.body;
-  const id = post.id;
-  fs.unlink(`data/${id}`, function (error) {
-    response.redirect(`/`);
-  });
-});
+// /topic으로 시작하는 주소들에게 topicRouter라는 이름의 미들웨어를 적용하겠다는 뜻이다.
+// /topic으로 인해 topic.js 파일에서 /topic 경로를 설정해줄 필요가 없다.
+app.use("/", indexRouter);
+app.use("/topic", topicRouter);
 
 app.use((request, response, next) => {
   response.status(404).send("Sorry can't find that!");
---
+++ routes/topic.js
@@ -0,0 +1,116 @@
+// router를 쓰는 이유는 서로 연관되어 있는 route들을 별도의 파일로 빼서 코드를 훨씬 보기 좋기 하기 위해서이다.
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const sanitizeHtml = require("sanitize-html");
+const template = require("../lib/template.js");
+
+router.get("/create", (request, response) => {
+  const title = "WEB - create";
+  const list = template.list(request.list);
+  const html = template.HTML(
+    title,
+    list,
+    `
+    <form action="/topic/create_process" method="post">
+        <p><input type="text" name="title" placeholder="title"></p>
+        <p>
+        <textarea name="description" placeholder="description"></textarea>
+        </p>
+        <p>
+        <input type="submit">
+        </p>
+    </form>
+      `,
+    ""
+  );
+  response.send(html);
+});
+
+// create_process 말고 create form에서 주소를 create만 주고 post 방식이기 때문에 create만 적어줘도 된다.
+// 이러면 get 방식이면 위에 코드가 실행되고 post 방식이면 아래의 코드가 실행된다.
+router.post("/create_process", (request, response) => {
+  const post = request.body;
+  const title = post.title;
+  const description = post.description;
+  fs.writeFile(`data/${title}`, description, "utf8", function (err) {
+    response.redirect(`/topic/${title}`);
+  });
+});
+
+router.get("/update/:pageId", (request, response) => {
+  const filteredId = request.params.pageId;
+  fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
+    const title = filteredId;
+    const list = template.list(request.list);
+    const html = template.HTML(
+      title,
+      list,
+      `
+        <form action="/topic/update_process" method="post">
+        <input type="hidden" name="id" value="${title}">
+        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+        <p>
+            <textarea name="description" placeholder="description">${description}</textarea>
+        </p>
+        <p>
+            <input type="submit">
+        </p>
+        </form>
+        `,
+      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
+    );
+    response.send(html);
+  });
+});
+
+router.post("/update_process", (request, response) => {
+  const post = request.body;
+  const id = post.id;
+  const title = post.title;
+  const description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function (error) {
+    fs.writeFile(`data/${title}`, description, "utf8", function (err) {
+      response.redirect(`/topic/${title}`);
+    });
+  });
+});
+
+router.post("/delete_process", (request, response) => {
+  const post = request.body;
+  const id = post.id;
+  fs.unlink(`data/${id}`, function (error) {
+    response.redirect(`/`);
+  });
+});
+
+router.get("/:pageId", (request, response, next) => {
+  fs.readFile(`data/${request.params.pageId}`, "utf8", function (err, description) {
+    if (err) {
+      next(err);
+    } else {
+      const title = request.params.pageId;
+      const sanitizedTitle = sanitizeHtml(title);
+      const sanitizedDescription = sanitizeHtml(description, {
+        allowedTags: ["h1"],
+      });
+      const list = template.list(request.list);
+      const html = template.HTML(
+        sanitizedTitle,
+        list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        `
+        <a href="/topic/create">create</a>
+        <a href="/topic/update/${sanitizedTitle}">update</a>
+        <form action="/topic/delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+        </form>
+        `
+      );
+      response.send(html);
+    }
+  });
+});
+
+module.exports = router;
